8a167a49f6fcd6b2e280f59870d36c59223466f0
Duy Hoang <hpduy@tma.com.vn>
2022-07-15 10:16:45 +0700

    create chapter 11

---
+++ 11TUT/controllers/authController.js
@@ -0,0 +1,59 @@
+const userDB = {
+  users: require('../model/users.json'),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+const bcrypt = require('bcrypt');
+
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const handleLogin = async (req, res) => {
+  const { user, pwd } = req.body;
+  if (!user || !pwd) {
+    return res
+      .status(400)
+      .json({ message: 'User name and password are required.' });
+  }
+  const foundUser = userDB.users.find((person) => person.username === user);
+  if (!foundUser) {
+    return res.sendStatus(401); // Unauthorized
+  }
+  // evaluate password
+  const math = await bcrypt.compare(pwd, foundUser.password);
+  if (math) {
+    //create JWTs
+    const accessToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: '30s' }
+    );
+    const refreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: '1d' }
+    );
+    // Saving refreshToken with current user
+    const otherUser = userDB.users.filter(
+      (person) => person.username !== foundUser.username
+    );
+    const currentUser = { ...foundUser, refreshToken };
+    userDB.setUsers([...otherUser, currentUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, '..', 'model', 'users.json'),
+      JSON.stringify(userDB.users)
+    );
+    res.cookie('jwt', refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.json({ accessToken });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+module.exports = { handleLogin };
